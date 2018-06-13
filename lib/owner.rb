5fa4fa8600b2b2ffa212c6523ae0a1a7b27be079
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-06-13 17:40:18 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species 
+  
+  @@owners = []
+  @@count = 0
+  
+  def initialize(species)
+    @name = name 
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = species 
+    @@count += 1
+    @@owners << self 
+  end 
+  
+  def say_species 
+		"I am a #{species}."
+ end 
+  
+  def buy_fish 
+  end 
+  
+  def buy_cat 
+  end 
+  
+  def buy_dog 
+  end 
+  
+  def walk_dogs 
+  end 
+  
+  def play_with_cats 
+  end 
+  
+  def feed_fish 
+  end 
+  
+  def sell_pets 
+  end 
+  
+  def list_pets 
+  end 
 end
