cae9ef8ae2be5332d26338dc191e57a1b1a7e908
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-06-13 17:59:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,6 +68,6 @@ end
 end 
   
   def list_pets 
-    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s) and #{pets[:cats].count} cat(s)."
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end 
 end
